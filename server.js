50b69b704651e03a210881a20e733bc8709bd4b0
Bhavuk <13174338+bhvk@users.noreply.github.com>
2018-02-18 10:39:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req, res){
@@ -18,7 +18,7 @@ app.get('/article-two', function(req, res){
 });
 
 app.get('/article-three', function(req, res){
-   res.sendFile('Article 3rd requested and response will be served here.'); 
+   res.send('Article 3rd requested and response will be served here.'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
