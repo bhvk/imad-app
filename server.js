0b70aef4aa530252c70df3526641235a2b401594
Bhavuk <13174338+bhvk@users.noreply.github.com>
2018-02-18 10:37:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-   res.sendFile('Article 1st requested and response will be served here.') 
+   res.sendFile('Article 1st requested and response will be served here.'); 
 });
 
 app.get('/article-two', function(req, res){
-   res.sendFile('Article 2nd requested and response will be served here.') 
+   res.sendFile('Article 2nd requested and response will be served here.'); 
 });
 
 app.get('/article-three', function(req, res){
-   res.sendFile('Article 3rd requested and response will be served here.') 
+   res.sendFile('Article 3rd requested and response will be served here.'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
