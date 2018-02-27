23d7c3cb35f195683838f78fb966c2b9da8c5a45
Bhavuk <13174338+bhvk@users.noreply.github.com>
2018-02-27 21:29:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,22 @@
 console.log('Loaded!');
+
+//change the main text below hasura's pet (madi) image to new text
+var element = document.getElementById("main-text");
+
+element.innerHTML = "Call me Madi";
+
+//animate the image
+
+var img = document.getElementById("madi");
+
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft += 2;
+    img.style.marginLeft = marginLeft+'px';
+    
+    }
+img.onclick =function(){
+    var interval = setInterval(moveRight, 50);
+    
+}
