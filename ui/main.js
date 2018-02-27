b09d8b02aeff5e3869fa5051d39b85eb36678b93
Bhavuk <13174338+bhvk@users.noreply.github.com>
2018-02-27 21:33:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,9 +14,8 @@ var marginLeft = 0;
 function moveRight(){
     marginLeft += 2;
     img.style.marginLeft = marginLeft+'px';
-    
-    }
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
-    
 }
+
+img.onclick = function(){
+    var interval = setInterval(moveRight, 50);   
+};
